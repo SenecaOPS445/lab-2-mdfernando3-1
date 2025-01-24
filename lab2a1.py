453d74dc40858d7375d5ace7482e701c08e2afa7
Marian Fernando <mdfernando3@mysenenca.ca>
2025-01-24 17:52:33 +0000

    trying to add lab2a & lab2a1.py

--- lab2a1.py
+++ lab2a1.py
@@ -2,6 +2,8 @@
 
 name = 'Jon'
 age = 20
-#print('Hi ' + name + ', you are ' + str(age) + ' years old.')
 colour = input("Type in a colour and press enter: ")
 print('The colour I typed in is: ' + colour)
+#print ('Hi ' + name + ', you are ' + str(age) + ' years old.')
+
+
